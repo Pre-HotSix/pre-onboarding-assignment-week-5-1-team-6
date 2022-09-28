00475cb6cf25af26710f525380a9b16817d65b54
MINYUKYUNG <minew0223@gmail.com>
2022-09-28 18:32:03 +0900

    🎨 Modify : styled-components 등 누락된 라이브러리 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from 'styled-components';
-import Router from './routes/route';
+import Router from './routes';
 import { GlobalStyle } from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle></GlobalStyle>
-      <Router></Router>
+      <GlobalStyle />
+      <Router />
     </ThemeProvider>
   );
 }
